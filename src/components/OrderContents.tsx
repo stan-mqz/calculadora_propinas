95f2021d945aa3377af2feea41d3c3d40a2e3e84
Stan <moscarstanley37@gmail.com>
2024-09-22 17:39:36 -0600

    OrderContents styles added

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -1,35 +1,39 @@
-import { OrderItem } from "../types/types"
+import { formatCurrency } from "../helpers";
+import { OrderItem } from "../types/types";
 
 type OrderContentsProps = {
+  order: OrderItem[];
+};
 
-  order: OrderItem[]
-
-}
-
-export default function ({order} : OrderContentsProps) {
+export default function ({ order }: OrderContentsProps) {
   return (
-
     <div>
-        <h2 className="font-black text-4xl">Consumo</h2>
+      <h2 className="font-black text-4xl">Consumo</h2>
 
-          <div className="space-y-3 mt-5">
+      <div className="space-y-3 mt-10">
+        {order.length === 0 ? (
+          <p>La orden esta vacía</p>
+        ) : (
+          order.map((item) => (
+            <div key={item.id} className="flex justify-between items-center border-t border-gray-200 py-5 last-of-type:border-b">
+              <div>
+                <p className="text-lg">
+                  {item.name} - {formatCurrency(item.price)}
+                </p>
 
-              {order.length === 0 ? 
-              
-                <p>La orden esta vacía</p>
-              
-              : (
-                order.map(item => (
-                  <div key={item.id}>
-                      <p>{item.name}</p>
-                  </div>
-                  
-                ))
-              )
-              
-              }
+                <p className="font-black">
+                  Cantidad : {item.quantity} -{" "}
+                  {formatCurrency(item.price * item.quantity)}
+                </p>
+              </div>
 
-          </div>
+              <button className="bg-red-600 h-8 w-8 rounded-full text-white font-black">
+                X
+              </button>
+            </div>
+          ))
+        )}
+      </div>
     </div>
-  )
+  );
 }
