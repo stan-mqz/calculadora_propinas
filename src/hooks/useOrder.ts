eab2e094d28f5cf45ea9873dabec0dda3c8f0e39
stan-mqz <moscarstanley37@gmail.com>
2024-09-23 14:16:25 -0600

    Comments added to useOrder hook

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -6,18 +6,26 @@ export default function useOrder() {
 
   //Creas la función, especificas que tipo de dato recibirá el parámetro
   const addItem = (item: MenuItem) => {
+    //Bucamos en el state de orden un elemento cuyo id, sea igual que la id del objeto que recibió la función
     const itemExists = order.find((orderItem) => orderItem.id === item.id);
 
+    //Si encuentra un elemento que cumpla la condición devolverá un truthy, sino, un falsy
     if (itemExists) {
-      //Si encuentra duplicados, obtenemos cual elemento es y operamos en base a eso
+
+      //Si se cumple la condición, creamos una copia del estado "order", como vamos a modificar uno de sus elementos usamos map. Le decimos que entregue un true o un false en caso la             condición que le pasamos se cumpla o no.
+
+      //Al elemento que cumpla la condición, al ser un objeto, creamos una copia exacta de él, y modificamos la propiedad quantity. Lo almacenamos en la variable updatedOrder
       const updatedOrder = order.map((orderItem) =>
         orderItem.id === item.id
           ? { ...orderItem, quantity: orderItem.quantity + 1 }
           : orderItem
+                                     
       );
+
+      //Pasamos updatedOrder como argumento a setOrder
       setOrder(updatedOrder);
     } else {
-      //Creamos una copia del objeto recibido y le añadimos la cantidad
+      //Si no se cumple la condición y entrega un fasly, creamos una copia del objeto recibido y le añadimos la cantidad
       //También podemos indicar explicítamente que el nuevo objeto será de tipo OrderItem
       const newItem: OrderItem = { ...item, quantity: 1 };
       setOrder([...order, newItem]);
